6f73af16d4d7b861ccccd54d880eedf9b0722220
cybertic <cybertic@gmail.com>
2026-03-17 14:30:41 +0000

    Update project files for week 1 lecture

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// Add JS here
+const generateBtn = document.getElementById('generate');
+const numbersDiv = document.getElementById('numbers');
+
+generateBtn.addEventListener('click', () => {
+    const numbers = [];
+    while (numbers.length < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        if (!numbers.includes(randomNumber)) {
+            numbers.push(randomNumber);
+        }
+    }
+    numbersDiv.textContent = numbers.join(', ');
+});
